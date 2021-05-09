2ff2262f2968cf9e4a3de0f7d32ae043af661570
rajesh <rajesh@rajeshs-MacBook-Pro.local>
2021-05-10 00:34:30 +0530

    upd

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # test
+
+python --version  
+
+python -m venv webenv
+source webenv/bin/activate
+
+scrapy genspider imdb www.imdb.com
+ 1020  scrapy crawl imdb
+ 1021  scrapy crawl imdb -o imdbdata.json -t json
--- webscaper.py
+++ webscaper.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 
-url = 'https://programwithus.com/learn/python/pip-virtualenv-mac'
-
 browser = webdriver.Chrome()
+
+# url = 'https://programwithus.com/learn/python/pip-virtualenv-mac'
+url = 'https://tech-with-tim.teachable.com/p/the-fundamentals-of-programming-with-python'
 
 browser.get(url)
 
-browser.find_element_by_xpath('/html/head/link[9]').click()
+# browser.find_element_by_xpath('/html/head/link[9]').click()
+browser.close()
